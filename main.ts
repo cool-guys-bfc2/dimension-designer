9dac535d41bc35dfaf5eb1a9c4e6328a16f4cdc6
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-07-31 18:55:48 -0400

    Add 'innocent' behavior and 'fireman' entity

--- main.ts
+++ main.ts
@@ -22,6 +22,31 @@ registerBlock({
   colors: { top: 0xc24e13, side: 0xc24e13, bottom: 0xc24e13 },
   solid: true, opaque: true, hardness: 2
 });
+registerBehavior({
+  name: "innocent",
+  onUpdate: (c) => {
+    if (c.distance < 8 && c.distance > 2) c.chase();
+    else c.wander();
+  },
+  onSpawn: (c) => { c.entity.memory.greeted = false; },
+  onPlayerNear: (c) => {
+    if (!c.entity.memory.greeted) {
+      c.entity.memory.greeted = true;
+      c.say(`A ${c.entity.def.displayName} says hello!`);
+    }
+  },
+  onDeath: (c) => {
+    if (c.entity.memory.greeted) {
+      c.say(`You killed him! He wanted to be your friend! <Awkard Silence>`);
+    }
+  },
+});
+registerEntity({
+  name: "fireman", displayName: "FireMan",
+  color: 0xd17f36, size: [0.7, 1.2, 0.4], speed: 1.6,
+  hostile: false, maxHealth: 20, behavior: "innocent",
+  loot: [{ item: "pork", min: 2, max: 4, chance: 1 }],
+});
 registerDimension({
   name: "nether",
   displayName: "Nether",
@@ -31,5 +56,6 @@ registerDimension({
   nostructures:false,
   surfaceBlock: "deathgrass",
   fillerBlock: "deathsoil",
-  stoneBlock: "deathstone"
+  stoneBlock: "deathstone",
+  entities: ["fireman",'pig','cow']
 });
